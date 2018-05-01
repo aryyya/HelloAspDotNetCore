beea82793862173163f91e4fd889837260eb70d6
aryyya <arya@live.ca>
2018-05-01 13:24:34 -0700

    Scaffold dotnet console application.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloAspDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
